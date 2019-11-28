e33e7c82899a5bbe3eed337451cc47f193021736
Jennice <colby1226@tinnos.com>
2019-11-28 17:05:39 +0900

    Modified ConverToDic function

--- python/can_matrix/can_matrix.py
+++ python/can_matrix/can_matrix.py
@@ -19,9 +19,9 @@ def excel_to_csv():
 #creating csv file
 excel_to_csv()
 
+#global variables
 file = 'can_matrix_csv.csv'
-
-def convert_to_dic(file):
+def ConvertToDic(file):
     #creating empty dictionary
     columns = defaultdict(list)
     #Going through each items of the rows
@@ -31,38 +31,49 @@ def convert_to_dic(file):
             for (k,v) in row.items():
                 columns[k].append(v)
     #Creating columns
-    Signal_Name = columns['Signal Name']
-    Start_Bit = columns['Start Bit\n(LSB)']
-    Signal_Size = columns['Signal Size']
-    Msg_ID = columns['Msg ID']
+    signal_name = columns['Signal Name']
+    start_bit = columns['Start Bit\n(LSB)']
+    signal_size = columns['Signal Size']
+    msg_id = columns['Msg ID']
+    value_des = columns['Value_Description']
 
     #Creating dictionary with Signal_Name as key
-    dic1 = dict(zip(Signal_Name,Start_Bit))
-    dic2 = dict(zip(Signal_Name,Signal_Size))
-    dic3 = dict(zip(Signal_Name,Msg_ID))
-
-    dic4 = defaultdict(list)
-    #combining values of the same key
-    for d in (dic1,dic2,dic3):
+    startbit_dic = dict(zip(signal_name,start_bit))
+    signalsize_dic = dict(zip(signal_name,signal_size))
+    msgid_dic = dict(zip(signal_name,msg_id))
+    temp_value_dic= dict(zip(signal_name,value_des))
+    sort_temp_value_dic = sorted(temp_value_dic.items())
+    #Creating list to append splited value description
+    words = []
+    #Split value descption by : and space
+    for i in range(len(sort_temp_value_dic)):
+        for j in range(1):
+            words.append(re.split('[:,"\n"]', sort_temp_value_dic[i][1]))
+    valuedes_dic = dict(zip(signal_name,words))
+    # combining values of the same key into dictionary
+    dictionary = defaultdict(list)
+    for d in (startbit_dic,signalsize_dic,msgid_dic,valuedes_dic):
         for key,value in d.items():
-            dic4[key].append(value)
-    return dic4
+            dictionary[key].append(value)
+
+    return dictionary
 
 #convert int into hex
-def int_hex(n):
+def IntHex(n):
     hex = '{n:x}'.format(n=n)
     if len(hex) & 1:
         hex = '0' + hex
     return hex
 
+#convert csv to a new table in dictionary type
+dic = ConvertToDic(file)
 # convert signal info into can data
-def makeCanData(Signal):
-    dic = convert_to_dic(file)
+def MakeCanData(Signal):
     values = dic[Signal]
     bit = values[0]
     size = values[1]
     id = values[2]
-    little_end = int_hex(2**int(bit))
+    little_end = IntHex(2**int(bit))
     little_end = wrap(little_end,2)
     big_end = list(reversed(little_end))
     big_end = ''.join(big_end)
@@ -74,7 +85,8 @@ def makeCanData(Signal):
     can_data = '0x' + can_data
     return can_data
 
-print(makeCanData('LFDoorStatus'))
-print(type(makeCanData('LFDoorStatus')))
-print(int_hex(268435456))
-
+#1.for(name: Signal)
+    #for(value: 2 ** signal size)
+        #MakeCanData(name,value)
+#2. signal, value, can id, can data
+#3. if (value != datamodel(signal,can id,can data) ERROR

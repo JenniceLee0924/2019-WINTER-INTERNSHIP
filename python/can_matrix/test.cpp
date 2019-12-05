5d4d4a4052a9b9876a736ed42a71f40868726b6a
sangjeong <sangjeong@tinnnos.com>
2019-12-05 13:11:33 +0900

    add gtest source file

---
+++ python/can_matrix/test.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <gtest/gtest.h>
+
+#include "CanDataModel.h"
+
+TEST(test_case_name, test_name) {
+
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
